180f15ab30d184eaa3bc7e175e93c5236dad8aa9
mionchinskiy <d.mionchinskiy@gmail.com>
2022-05-19 21:22:19 +0300

    Add ProfileView in Profile.storyboard

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by MACBOOK on 19.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthdate: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var descriptionOfPerson: UITextView!
+
+}
